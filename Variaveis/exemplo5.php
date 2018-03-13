7eb715a706aceec123660ae286ca0899636c4fd6
Silas Gon <silasidc@gmail.com>
2018-03-13 14:57:51 -0300

    Atualização diaria

---
+++ Variaveis/exemplo5.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Silas";
+
+function teste(){
+    global $nome;
+    echo $nome;
+}
+
+function teste2(){
+    $nome = "João";
+    echo $nome." Agora no teste2";
+}
+
+teste();
+teste2();
+?>
