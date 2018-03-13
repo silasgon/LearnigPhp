776edd8c8a5177d940c066217970bccf523302e7
Silas Gon <silasidc@gmail.com>
2018-03-13 17:49:33 -0300

    Operadores no php.

---
+++ Operadores/exemplo3.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+$b = 2;
+//Soma
+echo $a + $b;
+
+echo "<br/>";
+//subtração
+echo $a - $b;
+
+echo "<br/>";
+//Multiplicação
+echo $a * $b;
+
+echo "<br/>";
+//Divisão
+echo $a / $b;
+
+echo "<br/>";
+//Modulo (resto da divisão)
+echo $a % $b;
+
+echo "<br/>";
+//Exponenciação
+echo $a ** $b;
+
+?>
