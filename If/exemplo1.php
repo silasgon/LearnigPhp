d8e3dccb62f05e59a9d5cbaf3dcf60652bd31062
Silas Gon <silasidc@gmail.com>
2018-03-14 23:37:40 -0300

    If e operador ternario

--- If/exemplo1.php
+++ If/exemplo1.php
@@ -1,5 +1,24 @@
 <?php
 
+$qualASuaIdade = 75;
 
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if($qualASuaIdade < $idadeCrianca){
+
+    echo "É uma criança";
+}else if($qualASuaIdade < $idadeMaior){
+    echo "Adolecente";
+}else if($qualASuaIdade < $idadeMelhor){
+    echo "Adulto";
+}else{
+    echo "Idoso";
+}
+
+echo "<br/>";
+
+echo ($qualASuaIdade < $idadeMaior) ? "Menor de Idade" : "Maior de Idade";
 
 ?>
