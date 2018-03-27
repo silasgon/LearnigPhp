1aaa19fdbee8c7c0919210e76bd691038a0a36ff
Silas Gon <silasidc@gmail.com>
2018-03-27 17:49:23 -0300

    Metodos mágicos __construct, __destruct e __toString.

--- POO/exemplo3.php
+++ POO/exemplo3.php
@@ -1,1 +1,32 @@
 <?php
+
+class Endereco{
+
+    private $logradouro;
+    private $numero;
+    private $cidade;
+
+  public function __construct($a, $b, $c)
+  {
+      $this->logradouro = $a;
+      $this->numero = $b;
+      $this->cidade = $c;
+  }
+
+  public function __destruct()
+  {
+      // TODO: Implement __destruct() method.
+    //  var_dump("Destruir");
+  }
+
+  public function __toString()
+  {
+      // TODO: Implement __toString() method.
+      return $this->logradouro . ", " .$this->numero . ", " .$this->cidade;
+  }
+
+}
+
+$meuEndereco = new Endereco("Quadra 1306 SUl ALAMEDA 13A","09","Palmas");
+
+echo $meuEndereco;
