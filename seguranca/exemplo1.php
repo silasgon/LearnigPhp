fdbfbcdb14bbbb786d9d4ba2600d2949c02fbc5e
Silas Gon <silasidc@gmail.com>
2018-04-17 17:58:09 -0300

    Segurança em PHP

---
+++ seguranca/exemplo1.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+   $cmd = $_POST["cmd"];
+
+    echo "<pre>";
+
+    $comando = system("dir C:", $retorno);
+
+    echo "</pre>";
+}
+?>
+
+<form method="post">
+    <input type="text" name="cmd">
+    <button type="submit">Enviar</button>
+</form>
