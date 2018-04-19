4a49ccec67af6d0d1258618d579236526676d09c
Silas Gon <silasidc@gmail.com>
2018-04-19 17:40:07 -0300

    Segurança em PHP-recaptcha do google

---
+++ seguranca/cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silas.goncalves
+ * Date: 19/04/2018
+ * Time: 17:02
+ */
+
+$email = $_POST["inputEmail"];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query(array(
+    "secret"=>"6LenWFQUAAAAAMF2cQCriU3A5O6rb3eXOvrW7okY",
+    "response"=>$_POST["g-recaptcha-response"],
+    "remoteip"=>$_SERVER["REMOTE_ADDR"]
+)));
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+
+$recaptcha = json_decode(curl_exec($ch), true);
+
+curl_close($ch);
+
+if($recaptcha["success"] === true){
+    echo "OK: " . $_POST["inputEmail"];
+}else{
+    header("Location: exemplo4.php");
+}
---
+++ seguranca/exemplo4.php
@@ -0,0 +1,9 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<form action="cadastrar.php" method="post">
+
+    <div class="g-recaptcha" data-sitekey="6LenWFQUAAAAAAELpd8HBtdvaOTl9ViaU4iB819x"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+
+</form>
